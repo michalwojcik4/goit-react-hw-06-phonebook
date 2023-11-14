e8b6b97133d8bad721f5dbf35906f89e300b8356
michalwojcik4 <mwjcik4@gmail.com>
2023-11-14 20:20:52 +0100

    added local storage and used builder callback

--- src/app/reducers/contactsReducer.js
+++ src/app/reducers/contactsReducer.js
@@ -1,12 +1,22 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { addContact, deleteContact } from '../actions/actions';
 
-const initialState = [];
+const initialState = JSON.parse(localStorage.getItem('contacts')) || [];
 
-const contactsReducer = createReducer(initialState, {
-  [addContact]: (state, action) => [...state, action.payload],
-  [deleteContact]: (state, action) =>
-    state.filter(contact => contact.id !== action.payload),
+const contactsReducer = createReducer(initialState, builder => {
+  builder
+    .addCase(addContact, (state, action) => {
+      const updatedState = [...state, action.payload];
+      localStorage.setItem('contacts', JSON.stringify(updatedState));
+      return updatedState;
+    })
+    .addCase(deleteContact, (state, action) => {
+      const updatedState = state.filter(
+        contact => contact.id !== action.payload
+      );
+      localStorage.setItem('contacts', JSON.stringify(updatedState));
+      return updatedState;
+    });
 });
 
 export default contactsReducer;
