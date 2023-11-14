a0ff7e1fe3f78e3130590c31efb3320a53bb0260
michalwojcik4 <mwjcik4@gmail.com>
2023-11-14 20:55:19 +0100

    added filter the same number and nofify in contactsReducer

--- src/app/reducers/contactsReducer.js
+++ src/app/reducers/contactsReducer.js
@@ -1,13 +1,22 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { addContact, deleteContact } from '../actions/actions';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const initialState = JSON.parse(localStorage.getItem('contacts')) || [];
 
 const contactsReducer = createReducer(initialState, builder => {
   builder
     .addCase(addContact, (state, action) => {
+      const checkContact = state.find(
+        contact => contact.number === action.payload.number
+      );
+      if (checkContact) {
+        Notify.failure('There is a contact for this number in the phone book');
+        return state;
+      }
       const updatedState = [...state, action.payload];
       localStorage.setItem('contacts', JSON.stringify(updatedState));
+      Notify.success('Great! Contact has been added');
       return updatedState;
     })
     .addCase(deleteContact, (state, action) => {
@@ -15,6 +24,7 @@ const contactsReducer = createReducer(initialState, builder => {
         contact => contact.id !== action.payload
       );
       localStorage.setItem('contacts', JSON.stringify(updatedState));
+      Notify.info('Contact has been removed');
       return updatedState;
     });
 });
